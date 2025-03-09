d016d5e2806419b99425e82f571c0e16aaf8c059
kakha <k_kareanshvili@cu.edu.ge>
2025-03-09 23:49:54 +0400

    adding missing files

---
+++ AuthorizationService/Program.cs
@@ -0,0 +1,41 @@
+using AuthorizationService.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<RegistrationService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "http://keycloak:8080/realms/E-Commerce";
+        options.Audience = "my-api-client";
+        options.RequireHttpsMetadata = false;
+    });
+
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
