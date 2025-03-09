3af843b52e399a3a453b2c68d5fa959c91ccd25e
kakha <k_kareanshvili@cu.edu.ge>
2025-03-09 23:47:44 +0400

    added keycloak and AuthorizationService with costume registration logic

---
+++ AuthorizationService/Controllers/RegistrationController.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using AuthorizationService.Dtos;
+using AuthorizationService.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AuthorizationService.Controllers;
+
+[ApiController]
+public class RegistrationController : Controller
+{
+    private readonly RegistrationService _registrationService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RegistrationController"/> class.
+    /// </summary>
+    /// <param name="registrationService">Service responsible for user registration.</param>
+    public RegistrationController(RegistrationService registrationService)
+    {
+        _registrationService = registrationService;
+    }
+
+    /// <summary>
+    /// Registers a new user.
+    /// </summary>
+    /// <param name="CreateUserDto">User data transfer object containing registration details.</param>
+    /// <returns>An IActionResult indicating success or failure of registration.</returns>
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(CreateUserDto createUserDto)
+    {
+        var result = await _registrationService.Register(createUserDto);
+       
+        return result.Success ?  Ok(result) : BadRequest(result.Message);
+    }
+
+    
+    
+}
---
+++ AuthorizationService/Dtos/Response/ServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace AuthorizationService.Dtos.Response;
+
+public class ServiceResponse<T>
+{
+    public bool Success { get; set; } 
+    public string Message { get; set; }  
+    public T? Data { get; set; }
+}
